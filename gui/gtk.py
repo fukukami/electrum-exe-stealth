5548c912fb2ada382a62f1712513a0f824633120
fukukami <fukukami@vmail.me>
2014-06-14 18:14:34 -0400

    Add execoin support

--- gui/gtk.py
+++ gui/gtk.py
@@ -256,7 +256,7 @@ def run_network_dialog( network, parent ):
         host_entry.set_text("Not Connected")
     host_entry.show()
     host_box.pack_start(host_entry, False, False, 10)
-    add_help_button(host_box, 'The name, port number and protocol of your Electrum server, separated by a colon. Example: "ecdsa.org:50002:s". Some servers allow you to connect through http (port 80) or https (port 443)')
+    add_help_button(host_box, 'The name, port number and protocol of your Electrum server, separated by a colon. Example: "electrum.execoin.net:50012:s". Some servers allow you to connect through http (port 80) or https (port 443)')
     host_box.show()
 
     p_box = Gtk.HBox(False, 10)
